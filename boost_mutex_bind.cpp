7d13c2039624f90d8e0d743519b7e377af260098
MichaelWang1028 <gucaswanghy@gmail.com>
2018-05-25 11:13:55 +0800

    boost mutex and boost bind

---
+++ boost_mutex_bind.cpp
@@ -0,0 +1,24 @@
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
+
+boost::mutex io_mutex;
+
+void count(int id)
+{
+        for (int i = 0; i < 10; ++i)
+        {
+                boost::mutex::scoped_lock lock(io_mutex);
+                std::cout << id << ": " << i << std::endl;
+        }
+}
+
+int main(int argc, char* argv[])
+{
+        boost::thread thrd1(boost::bind(&count, 1));
+        boost::thread thrd2(boost::bind(&count, 2));
+        thrd1.join();
+        thrd2.join();
+        return 0;
+}
